0d4e12f3d71562875bfdbf6193c18efbc29e806f
Zacky Achmad <zckyachmd@gmail.com>
2023-02-12 10:42:38 +0700

    Publish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html", "./src/**/*.{js}"],
+  content: ["./public/**/*.html"],
   theme: {
     extend: {},
   },
